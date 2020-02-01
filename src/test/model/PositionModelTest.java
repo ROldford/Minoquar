dc88dbac88038800f33771ee5fd65aeb07f05512
Ryan Oldford <ryan.oldford@gmail.com>
2020-01-31 21:01:43 -0800

    Improved testing on PositionModel

--- src/test/model/PositionModelTest.java
+++ src/test/model/PositionModelTest.java
@@ -1,15 +1,31 @@
 package model;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PositionModelTest {
+    private PositionModel position;
+    private int posX;
+    private int posY;
+
+    @BeforeEach
+    public void beforeEach() {
+        this.posX = 6;
+        this.posY = 28;
+        this.position = PositionModel.createNewInstance(posX, posY);
+    }
 
     @Test
     public void testInit() {
-        PositionModel position = PositionModel.createNewInstance(6, 28);
-        assertEquals(6, position.getX());
-        assertEquals(28, position.getY());
+        assertEquals(posX, position.getX());
+        assertEquals(posY, position.getY());
+    }
+
+    @Test
+    public void testToString() {
+        String expected = String.format("(%d, %d)", posX, posY);
+        assertTrue(position.toString().equals(expected));
     }
 }
