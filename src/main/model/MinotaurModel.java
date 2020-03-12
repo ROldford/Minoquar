e5d18df57068c7b8d89f0775e61a76f45eb1cb53
Ryan Oldford <ryan.oldford@gmail.com>
2020-03-12 09:40:07 -0700

    Changed GameEntities to increase flexibility and avoid inheritance

--- src/main/model/MinotaurModel.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public class MinotaurModel extends GameEntity {
-    public static final char MINO_CHAR = "♈".charAt(0);
-
-    public MinotaurModel(PositionModel start) {
-        super(start);
-    }
-}
