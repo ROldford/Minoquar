999f5de607f20c6e43973d311ae4d589f9bccd3a
Ryan Oldford <ryan.oldford@gmail.com>
2020-03-14 16:35:55 -0700

    Finalized game control panel layout

--- src/main/ui/GameControlPanel.java
+++ src/main/ui/GameControlPanel.java
@@ -1,10 +1,12 @@
 package ui;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class GameControlPanel  extends JPanel {
+public class GameControlPanel extends JPanel {
     public static final String DEFAULT_MESSAGE_LABEL = "Your move!";
     public static final String BAD_MOVE_MESSAGE_LABEL = "That's not a valid move. Try again!";
     public static final String GAME_LOSS_MESSAGE_LABEL = "Sorry, you lost";
@@ -15,16 +17,17 @@ public class GameControlPanel  extends JPanel {
     private GameUI gameUI;
 
     private static final String QUIT_BUTTON_LABEL = "Quit Game";
+    private static final EmptyBorder MESSAGE_LABEL_PADDING = new EmptyBorder(0, 5, 0, 5);
 
     public GameControlPanel(GameUI gameUI) {
         this.gameUI = gameUI;
-        setLayout(new BoxLayout(this, BoxLayout.LINE_AXIS));
+        setLayout(new BorderLayout());
         // set up components
         this.quitButton = setUpQuitButton();
         this.messageLabel = setUpMessageLabel();
         // add components
-        add(quitButton);
-        add(messageLabel);
+        add(quitButton, BorderLayout.WEST);
+        add(messageLabel, BorderLayout.EAST);
     }
 
     private JButton setUpQuitButton() {
@@ -37,7 +40,9 @@ public class GameControlPanel  extends JPanel {
     }
 
     private JLabel setUpMessageLabel() {
-        return new JLabel(DEFAULT_MESSAGE_LABEL);
+        JLabel messageLabel = new JLabel(DEFAULT_MESSAGE_LABEL);
+        messageLabel.setBorder(MESSAGE_LABEL_PADDING);
+        return messageLabel;
     }
 
     public void updateMessageLabel(String message) {
