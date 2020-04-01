99108c67b1e86be5105fb5ad0092f481405361ed
Ryan Oldford <ryan.oldford@gmail.com>
2020-03-31 18:14:14 -0700

    Removed unneeded exceptions, simplified InvalidMazeSaveDataException to match standard library style

--- src/main/exceptions/InvalidMazeSaveDataException.java
+++ src/main/exceptions/InvalidMazeSaveDataException.java
@@ -1,14 +1,13 @@
 package exceptions;
 
+// TODO: document this
 public class InvalidMazeSaveDataException extends Exception {
-    public static final String DEFAULT_MESSAGE = "Save data contains invalid character";
-    public static final String MESSAGE_TEMPLATE = DEFAULT_MESSAGE + " %s";
 
-    public InvalidMazeSaveDataException() {
-        super(DEFAULT_MESSAGE);
+    public InvalidMazeSaveDataException(String s) {
+        super(s);
     }
 
-    public InvalidMazeSaveDataException(char invalidChar) {
-        super(String.format(MESSAGE_TEMPLATE, Character.toString(invalidChar)));
+    public InvalidMazeSaveDataException(String s, Throwable cause) {
+        super(s, cause);
     }
 }
